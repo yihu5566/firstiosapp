e7abe6b1753854497f2cb79b233013d7edf73383
ludongfang <ldf_5566@163.com>
2023-12-20 13:29:14 +0800

    web view 增加加载进度条

--- myfirstiosapp/NewsDetails/NewsDetailsViewController.m
+++ myfirstiosapp/NewsDetails/NewsDetailsViewController.m
@@ -10,19 +10,34 @@
 
 @interface NewsDetailsViewController ()<WKNavigationDelegate>
 @property(nonatomic,strong,readwrite) WKWebView *webView;
+@property(nonatomic,strong,readwrite) UIProgressView *progressView;
 
 @end
 
 @implementation NewsDetailsViewController
+- (void) dealloc{
+    [self.webView removeObserver:self forKeyPath:@"estimatedProgress"];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.webView = [[WKWebView alloc]initWithFrame:CGRectMake(0, 88, self.view.frame.size.width, self.view.frame.size.height-88)];
+    
+    [self.view addSubview:({
+        self.webView = [[WKWebView alloc]initWithFrame:CGRectMake(0, 88, self.view.frame.size.width, self.view.frame.size.height-88)];
+        self.webView.navigationDelegate=self;
+        self.webView;
+    })];
+    
     [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"https://www.baidu.com/"]]];
+
+    [self.webView addObserver:self forKeyPath:@"estimatedProgress" options:NSKeyValueObservingOptionNew context:nil];
     
-    self.webView.navigationDelegate=self;
+    [self.view addSubview:({
+        self.progressView = [[UIProgressView alloc]initWithFrame:CGRectMake(0, 98, self.view.frame.size.width, 20)];
+        self.progressView;
+    })];
     
-    [self.view addSubview:self.webView];
+    
 }
 
 
@@ -31,7 +46,7 @@
 - (void)webView:(WKWebView *)webView decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler WK_SWIFT_ASYNC(3){
 
     decisionHandler(WKNavigationActionPolicyAllow);
-    NSLog(@"decidePolicyForNavigationAction");
+//    NSLog(@"decidePolicyForNavigationAction");
     
 }
 
@@ -39,6 +54,13 @@
     NSLog(@"didFinishNavigation");
 
 }
+
+# pragma mark - observer
+- (void)observeValueForKeyPath:(nullable NSString *)keyPath ofObject:(nullable id)object change:(nullable NSDictionary<NSKeyValueChangeKey, id> *)change context:(nullable void *)context{
+    NSLog(@"");
+    self.progressView.progress = self.webView.estimatedProgress;
+}
+
 
 
 @end
