023dab2021671935a2e908ca0402b43ea83c0203
ludongfang <ldf_5566@163.com>
2024-03-14 16:42:32 +0800

    去掉scene 相关多窗口

--- myfirstiosapp/AppDelegate.m
+++ myfirstiosapp/AppDelegate.m
@@ -6,6 +6,12 @@
 //
 
 #import "AppDelegate.h"
+#import "DFSplash.h"
+#import "NewsViewController.h"
+#import "RecommendViewController.h"
+#import "SceneDelegate.h"
+#import "VideoViewController.h"
+#import "MineViewController.h"
 
 @interface AppDelegate ()
 
@@ -16,25 +22,47 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+
+    UITabBarController *tabBarController =  [[UITabBarController alloc]init];
+
+    NewsViewController *viewController = [[NewsViewController alloc]init];
+
+    VideoViewController *videoController = [[VideoViewController alloc]init];
+
+    RecommendViewController *recommendController = [[RecommendViewController alloc]init];
+
+    MineViewController *mineViewController = [[MineViewController alloc]init];
+    
+    [tabBarController setViewControllers:@[viewController, videoController, recommendController, mineViewController]];
+
+    UINavigationController *uiNavigationController1 =  [[UINavigationController alloc]initWithRootViewController:tabBarController];
+
+    self.window.rootViewController = uiNavigationController1;
+    [self.window makeKeyAndVisible];
+    [self.window addSubview:({
+        DFSplash *splash = [[DFSplash alloc]initWithFrame:self.window.bounds];
+        splash;
+    })];
     return YES;
 }
 
 
-#pragma mark - UISceneSession lifecycle
-
-
-- (UISceneConfiguration *)application:(UIApplication *)application configurationForConnectingSceneSession:(UISceneSession *)connectingSceneSession options:(UISceneConnectionOptions *)options {
-    // Called when a new scene session is being created.
-    // Use this method to select a configuration to create the new scene with.
-    return [[UISceneConfiguration alloc] initWithName:@"Default Configuration" sessionRole:connectingSceneSession.role];
-}
-
-
-- (void)application:(UIApplication *)application didDiscardSceneSessions:(NSSet<UISceneSession *> *)sceneSessions {
-    // Called when the user discards a scene session.
-    // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-    // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-}
+//#pragma mark - UISceneSession lifecycle
+//
+//
+//- (UISceneConfiguration *)application:(UIApplication *)application configurationForConnectingSceneSession:(UISceneSession *)connectingSceneSession options:(UISceneConnectionOptions *)options {
+//    // Called when a new scene session is being created.
+//    // Use this method to select a configuration to create the new scene with.
+//    return [[UISceneConfiguration alloc] initWithName:@"Default Configuration" sessionRole:connectingSceneSession.role];
+//}
+//
+//
+//- (void)application:(UIApplication *)application didDiscardSceneSessions:(NSSet<UISceneSession *> *)sceneSessions {
+//    // Called when the user discards a scene session.
+//    // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+//    // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+//}
 
 
 @end
