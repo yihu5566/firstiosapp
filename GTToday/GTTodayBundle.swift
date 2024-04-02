63e06e50f16d8d9c077247affe3f8b74862ae832
ludongfang <ldf_5566@163.com>
2024-04-02 15:34:38 +0800

    增加extension 和 app group

---
+++ GTToday/GTTodayBundle.swift
@@ -0,0 +1,16 @@
+//
+//  GTTodayBundle.swift
+//  GTToday
+//
+//  Created by dongfang lu on 2024/4/2.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct GTTodayBundle: WidgetBundle {
+    var body: some Widget {
+        GTToday()
+    }
+}
